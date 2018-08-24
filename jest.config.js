9c20c3bad5a5a2f570c4826b3e5c10121c9c5e62
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2018-08-24 18:12:53 +0200

    fix testMatch specification

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
     "**/__tests__/*.+(ts|tsx|js)",
-    "**/__spec__/*.+(ts|tsx|js)",
+    "**/*.spec.+(ts|tsx|js)",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
