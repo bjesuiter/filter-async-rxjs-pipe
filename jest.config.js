f04acd337ddd4ea242844a3b6cb5875397f17de2
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2018-08-24 18:00:18 +0200

    adds spec.ts testMatch expression

--- jest.config.js
+++ jest.config.js
@@ -57,7 +57,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
-      "tsConfigFile": "tsconfig.json"
+      "tsConfigFile": "dev.tsconfig.json"
     }
   },
 
@@ -137,7 +137,8 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/*.+(ts|tsx|js)"
+    "**/__tests__/*.+(ts|tsx|js)",
+    "**/__spec__/*.+(ts|tsx|js)",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
