3c0ece5b9962c29591d95c25fa890b1d5d4c04c1
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2018-08-27 17:41:08 +0200

    * re-adds test for filterByPromise (filterAsync) parallel returning correct number of elements
    * add test for showing that filterByPromise parallel runs indeed in parallel (currently failing)

--- src/lib/filter-async.spec.ts
+++ src/lib/filter-async.spec.ts
@@ -54,3 +54,55 @@ describe('Filter Async Sequential', () => {
     });
 });
 
+
+describe('Filter Async Parallel', () => {
+
+    it('Filter returns correct elements', (done) => {
+        const comparableResult = [2, 4, 6, 8, 10];
+
+        range(1, 10)
+            .pipe(
+                filterByPromise((value) => Promise.resolve(value % 2 === 0), true),
+                bufferCount(5)
+            )
+            .subscribe((filterResult: number[]) => {
+                expect(filterResult).toEqual(
+                    // arrayContaining must be used here, since the order of elements is not preserved with filterAsyncParallel
+                    expect.arrayContaining(comparableResult)
+                );
+                done();
+            });
+    });
+
+
+    it('Filter runs parallel', (done) => {
+        const comparableResult = ['d', 'c', 'b', 'a'];
+
+        of(...['a', 'b', 'c', 'd'])
+            .pipe(
+                filterByPromise((value) => {
+
+                    switch (value) {
+                        case 'a':
+                            // delay the filter promise about 3 seconds to increase chance of b returning before a
+                            return promiseDelay(moment.duration(3, 's').asMilliseconds(), true);
+                        case 'b':
+                            return promiseDelay(moment.duration(2, 's').asMilliseconds(), true);
+                        case 'c':
+                            return promiseDelay(moment.duration(1, 's').asMilliseconds(), true);
+                        case 'd':
+                            return Promise.resolve(true);
+                        default:
+                            // ignore all different values
+                            return Promise.resolve(false);
+                    }
+                }, true),
+                bufferCount(4)
+            )
+            .subscribe((elements: string[]) => {
+                expect(elements).toEqual(comparableResult);
+            }, console.log, done);
+    }, moment.duration(10, 's').asMilliseconds());
+
+});
+
